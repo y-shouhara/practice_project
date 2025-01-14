20564724b5e81e18c8b11a4dd4df8a1e8bebda9d
y-shohara <y-shohara@seassist.co.jp>
2025-01-14 15:10:14 +0900

    3.クラス課題(04修正)

--- src/main/java/practice/practice04/AnimalTest.java
+++ src/main/java/practice/practice04/AnimalTest.java
@@ -4,12 +4,12 @@ public class AnimalTest {
 
 	public static void main(String[] args) {
 		Dog dog = new Dog("ポチ", 2, "白");
+		Sparrow sparrow = new Sparrow("チュン", 1, true);
 		dog.ShowInfo();
 		dog.cry();
 
 		System.out.println("------------------------");
 
-		Sparrow sparrow = new Sparrow("チュン", 1, true);
 		sparrow.showInfo();
 		sparrow.cry();
 	}
